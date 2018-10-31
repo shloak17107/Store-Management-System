89d0db84572a34353b18df767cb47bc66e3af5b8
mohit17067 <32643701+Mohit17067@users.noreply.github.com>
2018-10-31 18:25:38 +0530

    Created SuperUser_main file.

--- src/model/Main.java
+++ src/model/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/fxml/store_admin.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/fxml/SuperUser_main.fxml"));
 			Scene scene = new Scene(root,800,500);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
