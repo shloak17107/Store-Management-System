7d07bdeb58330f4f5669a1ab235f588d65d2aa73
Shloak <31565271+shloak17107@users.noreply.github.com>
2018-11-03 02:23:05 +0530

    Fixed file names. Edited & created fxml files. Improved Cart page.
    Fixed names of fxml files to incorporate proper camel casing.
    Edited some old fxml files, created some new ones.
    Implemented appropriate GUI for Cart page including scroll functionality.

--- src/model/Main.java
+++ src/model/Main.java
@@ -7,15 +7,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/fxml/Categories_endUser.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/fxml/"));
 			Scene scene = new Scene(root,800,500);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
